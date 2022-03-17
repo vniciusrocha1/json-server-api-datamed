76a73ea9253cd1f34979c23a794021c2682754b0
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-03-17 20:20:59 -0300

    Testes

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const jsonServer = require("json-server");
-const auth = require("json-server-auth");
-const cors = require("cors");
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
 const port = process.env.PORT || 3001;
 
 const app = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 
 app.db = router.db;
 
 const rules = auth.rewriter({
-  users: 600,
+	users: 600,
+	pessoa: 644,
+	consulta: 644,
 });
 
 app.use(cors());
@@ -18,6 +20,6 @@ app.use(auth);
 app.use(router);
 app.listen(port);
 
-console.log("Server is running on port:", port);
+console.log('Server is running on port:', port);
 
 /* A senha do Kenzinho é 123456 */
