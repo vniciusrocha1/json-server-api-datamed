7b595ef69c12154d8fec021433b2b1f92ee7db6e
Vinicius Rocha <vinicius.rocha.mobile@gmail.com>
2022-03-17 19:18:32 -0400

    Build:Finalizando a estrutura inicial

--- server.js
+++ server.js
@@ -2,22 +2,19 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-
 app.db = router.db;
-
 const rules = auth.rewriter({
-  users: 600,
+    users: 600,
+    dados_usuarios: "dados_usuarios",
+    vinculo_medico_paciente: 640,
+    consultas: 640,
+    diagnosticos: 640,
 });
-
 app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
 app.listen(port);
-
 console.log("Server is running on port:", port);
-
-/* A senha do Kenzinho é 123456 */
